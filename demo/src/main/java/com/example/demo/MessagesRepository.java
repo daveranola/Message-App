c706552479c4ed70ef679f779267e410a8df99ac
XxdrixX <daveranola351@gmail.com>
2025-09-19 22:59:58 +0100

    switched to Jpa repository

--- demo/src/main/java/com/example/demo/MessagesRepository.java
+++ demo/src/main/java/com/example/demo/MessagesRepository.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.MessagesRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface MessagesRepository extends CrudRepository<Messages, Integer> {
+public interface MessagesRepository extends JpaRepository<Messages, Integer> {
 
-}
+}   
