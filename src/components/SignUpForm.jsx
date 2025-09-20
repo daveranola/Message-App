420470468c9ab6c542071fbc3ef5e6343c1460b1
XxdrixX <daveranola351@gmail.com>
2025-09-20 10:38:52 +0100

    validation for SignUpForm.jsx

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,15 +5,44 @@ function SignUpForm( { addUser } ) {
   const [signUpPassword, setSignUpPassword] = useState('');
   const [signUpEmail, setSignUpEmail] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // stop browser reload wgen form is submitted
+    addUser(signUpName, signUpEmail, signUpPassword);
+  };
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <input placeholder="Name" value={signUpName} onChange={e => setSignUpName(e.target.value)} />
-      {/* <>e.target.value = value inside element in this case input</>  */}
-      <input placeholder="Email" value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} />
-      <input placeholder='Password' value={signUpPassword} onChange={e => setSignUpPassword(e.target.value)} />
-      <button onClick={() => addUser(signUpName, signUpEmail, signUpPassword)}>Sign Up</button>
-    </div>
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        
+        <input 
+          placeholder="Name" 
+          value={signUpName} 
+          onChange={(e) => setSignUpName(e.target.value)} 
+          required
+          maxLength={20} //name cannot exceed 20 characters
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={signUpEmail}
+          onChange={(e) => setSignUpEmail(e.target.value)}
+          required
+          pattern=".+@gmail\.com"
+        />
+
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={signUpPassword} 
+          onChange={(e) => setSignUpPassword(e.target.value)} 
+          required
+          minLength={6} //must be at least 6 characters long
+        />
+
+        <button type="submit">Sign Up</button>
+    </form>
+
   );
 }
 
